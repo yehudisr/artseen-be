f7dda9935364d3ec300f1d86cbeed8c019dd213e
dapeshcode <ampeccia@gmail.com>
2021-04-20 12:27:33 -0500

    create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+    def index
+        @comments = Comment.all
+        render json: @comments
+    end 
+    def create
+        @comment = Comment.create(content: params[:content], event_id: params[:event_id], user_id: params[:user_id])
+        render json: @comment
+    end 
 end
