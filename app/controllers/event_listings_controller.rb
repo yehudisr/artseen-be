ad3d877ba1d45de4e664b49988edd663132c06e5
dapeshcode <ampeccia@gmail.com>
2021-04-21 01:05:05 -0500

    changes

--- app/controllers/event_listings_controller.rb
+++ app/controllers/event_listings_controller.rb
@@ -1,7 +1,7 @@
 class EventListingsController < ApplicationController
 
     def create 
-        @event_listing = EventListing.create(user_id: params[:user_id], event_id: params[:event_id])
+        @event_listing = EventListing.create(user_id: params[:user_id], event_id: params[:event_id], saved: params[:saved] , seen: params[:seen] , booked: params[:booked])
         render json: @event_listing
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,16 @@ class UsersController < ApplicationController
 
     def login 
         @user = User.find_by(email: params[:email])
-        render json: @user
+        if @user && @user.password_digest === params[:password]
+            render json: @user
+        else
+            render json: {errors: ["Invalid username or password"]}, status: :unauthorized
+        end 
     end
 
-    
+    def create
+        @user = User.create(email: params[:email], password: params[:password], firstname: params[:firstname], lastname: params[:lastname]) 
+        render json: @user
+    end 
 
 end
